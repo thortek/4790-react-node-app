d7f5190211b9a79abf17875210e5ebfbdd83a143
Thor Anderson <ta.anderson@gmail.com>
2021-03-18 07:27:55 -0600

    Relative paths for API calls

--- client/src/views/MovieList.js
+++ client/src/views/MovieList.js
@@ -81,7 +81,7 @@ const MovieList = () => {
     setDeleteOpen(false)
     console.log(selectedMovie._id)
     try {
-      await axios.delete(`https://localhost:${port}/movie/delete`, {
+      await axios.delete(`/movie/delete`, {
         data: {
           movieId: selectedMovie._id,
         },
@@ -103,7 +103,7 @@ const MovieList = () => {
 
   const handleUpdate = async (values) => {
     try {
-      const result = await axios.put(`https://localhost:${port}/movie/update`, {
+      const result = await axios.put(`/movie/update`, {
         data: {
           movieId: values.id,
           title: values.title,
@@ -133,7 +133,7 @@ const MovieList = () => {
 
   const fetchMovies = async () => {
     try {
-      const movies = await axios.get(`https://localhost:${port}/movie`)
+      const movies = await axios.get(`/movie`)
       setMovieList(movies.data)
     } catch (err) {
       console.error(err)
